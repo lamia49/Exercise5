9035c8a0b90c1ea7aac2df4082ecb99de30301cf
lamia49 <80590112+lamia49@users.noreply.github.com>
2024-02-18 18:15:20 +0300

    Add files via upload

---
+++ Exercies4/src/Main.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    //Qustion1:
+//    public static int smallest(int num1 ,int num2,int num3){
+//        ArrayList<Integer>numbers=new ArrayList<Integer>(3);
+//        numbers.add(num1);
+//        numbers.add(num2);
+//        numbers.add(num3);
+//        int smallest=numbers.get(0);
+//        for( int s:numbers){
+//            if(smallest< numbers.indexOf(s)){
+//                smallest=s;
+//            System.out.print("The smallest value is:"+s);
+//            }}
+//        return smallest;
+//    }
+
+    //Qustion2
+
+//    public static int check(int num){
+//        if (num == 0 ) {
+//            System.out.print(" zero");
+//        }else if (num>0){
+//            System.out.print(" Positiv");
+//        } else if (num<0) {
+//            System.out.print(" Negative");
+//        }
+//
+//        return num;
+//    }
+
+
+
+//Quastion3
+
+
+//   public static String checkpass(String password){
+//       Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+//       Matcher hasSpecial = special.matcher(password);
+//       Pattern pattern = Pattern.compile("\\d");
+//       Matcher matcher = pattern.matcher(password);
+//       if (password.length()<10)
+//           System.out.println("the password hve at lest ten chracter");
+//        else if (hasSpecial.find())
+//           System.out.println("the password must have only Digtil and charcter");
+//    else if(matcher.find())
+//           System.out.println("password is valid "+password);
+//    else
+//           System.out.println("password must have two digit at leastbs");
+//
+//     return password;
+//       }
+
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number1:");
+//        int number1 = input.nextInt();
+//        System.out.print("Enter number2:");
+//        int number2 = input.nextInt();
+//        System.out.print("Enter number3:");
+//        int number3 = input.nextInt();
+
+
+//        System.out.println("Enter your password: ");
+//        String s=input.nextLine();
+//System.out.println(checkpass(s));
+
+
+
+//       System.out.println(smallest(number1,number2,number3));
+//        System.out.println(check(number1));
+//        System.out.println(check(number2));
+//        System.out.println(check(number3));
+
+    }
+    }
